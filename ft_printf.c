e7ec136ce3c9407cca84f92f43a28f324f5629b1
wizzy06 <c.paris93@hotmail.fr>
2017-01-29 02:44:03 +0100

    Ajout du projet ft_printf

---
+++ ft_printf.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cparis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/28 23:52:27 by cparis            #+#    #+#             */
+/*   Updated: 2017/01/29 01:50:26 by cparis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void		ft_printf(const char *format, ...)
+{
+	va_list ap;
+	int nb;
+
+	nb = 0;
+	va_start(ap, format);
+	while (nb != 4)
+	{
+		char *n;
+
+		n = va_arg(ap, char *);
+		printf("%s.\n", n);
+		++nb;
+	}
+	va_end(ap);
+}
+
+void	format(const char *format, ...)
+{
+	va_list ap;
+	va_list ap2;
+	char c;
+	char *s;
+	int d;
+
+	va_start(ap, format);
+	va_copy(ap2, ap);
+	while (*format)
+	{
+		if(format == "s")
+		{
+			s = va_arg(ap, char *);
+			printf("string %s\n", s);
+			break;
+		}
+		if( format == 'd')
+		{
+			d = va_arg(ap, int);
+			printf("int %d\n", d);
+			break;
+		}
+		if(format == 'c')
+		{
+			c = va_arg(ap, int);
+			printf("char %c\n", c);
+			break;
+		}
+	}
+	va_end(ap);
+	va_end(ap2);
+}
+
+int main(void)
+{
+	ft_printf("\n","Hello", "Bea", "J'te", "Baise");
+	return (0);
+}
